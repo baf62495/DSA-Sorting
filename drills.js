490d0fa75616cc6d5514798d05c8ef9bef365607
baf62495 <baf62495@aol.com>
2020-02-20 18:01:56 -0500

    finished 3

--- drills.js
+++ drills.js
@@ -1,8 +1,6 @@
 // 1. Understanding merge sort
 // Given the following list of numbers 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
 
-// What is the resulting list that will be sorted after 3 recursive calls to mergesort?
-
 // [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40]
 
 // [21, 1, 26, 45, 29, 28, 2, 9] [16, 49, 39, 27, 43, 34, 46, 40]
@@ -13,10 +11,17 @@
 
 // [21] [1] [26] [45] [29] [28] [2] [9] [16] [49] [39] [27] [43] [34] [46] [40]
 
-// 1a. [21, 1]
-// 1b. [16, 49, 39, 27, 43, 34, 46, 40]
-// 1c. [21] [1]
-// 1d. ??
+// 1a. What is the resulting list that will be sorted after 3 recursive calls to mergesort?
+// [21, 1]
+
+// 1b. What is the resulting list that will be sorted after 16 recursive calls to mergesort?
+// [16, 49, 39, 27, 43, 34, 46, 40]
+
+// 1c. What are the first 2 lists to be merged?
+// [21] [1]
+
+// 1d. Which two lists would be merged on the 7th merge?
+// ??
 
 // 2. Understanding quicksort
 
@@ -38,3 +43,144 @@
 
 // When using the last item on the list as a pivot: [10, 3, 14, 17, 13, 15, 19, 16, 9, 12]
 // When using the first item on the list as a pivot: [13, 10, 14, 15, 19, 17, 3, 16, 9, 12]
+
+// 3. Write a function qSort that sorts a dataset using the quicksort algorithm. The dataset to sort is:
+// 89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5.
+
+const DATA = [
+  89,
+  30,
+  25,
+  32,
+  72,
+  70,
+  51,
+  42,
+  25,
+  24,
+  53,
+  55,
+  78,
+  50,
+  13,
+  40,
+  48,
+  32,
+  26,
+  2,
+  14,
+  33,
+  45,
+  72,
+  56,
+  44,
+  21,
+  88,
+  27,
+  68,
+  15,
+  62,
+  93,
+  98,
+  73,
+  28,
+  16,
+  46,
+  87,
+  28,
+  65,
+  38,
+  67,
+  16,
+  85,
+  63,
+  23,
+  69,
+  64,
+  91,
+  9,
+  70,
+  81,
+  27,
+  97,
+  82,
+  6,
+  88,
+  3,
+  7,
+  46,
+  13,
+  11,
+  64,
+  76,
+  31,
+  26,
+  38,
+  28,
+  13,
+  17,
+  69,
+  90,
+  1,
+  6,
+  7,
+  64,
+  43,
+  9,
+  73,
+  80,
+  98,
+  46,
+  27,
+  22,
+  87,
+  49,
+  83,
+  6,
+  39,
+  42,
+  51,
+  54,
+  84,
+  34,
+  53,
+  78,
+  40,
+  14,
+  5
+];
+
+function swap(array, i, j) {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+}
+
+function qSort(array, start = 0, end = array.length) {
+  if (start >= end) {
+    return array;
+  }
+
+  const middle = partition(array, start, end); // partition the array into 2 halves around a pivot value
+
+  array = qSort(array, start, middle); // recursively sort half until length of 0 or 1
+  array = qSort(array, middle + 1, end); // recursively sort half until length of 0 or 1
+
+  return array;
+}
+
+function partition(array, start, end) {
+  const pivot = array[end - 1]; // pivot is final value in the array
+  let j = start;
+
+  for (let i = start; i < end - 1; i++) {
+    if (array[i] <= pivot) {
+      swap(array, i, j);
+      j++;
+    }
+  }
+  swap(array, end - 1, j);
+  return j;
+}
+
+console.log(qSort(DATA));
